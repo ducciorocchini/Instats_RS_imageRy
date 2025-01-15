f3e8ebcd91ba5c5d4a3c408a048ffafed3194f67
Duccio Rocchini <ducciorocchini@gmail.com>
2025-01-15 17:11:26 +0100

    Update Session3_01_external_data.R

--- Code/Session3_01_external_data.R
+++ Code/Session3_01_external_data.R
@@ -2,13 +2,21 @@
 
 library(terra)
 
-setwd("/home/duccio/Downloads")
+setwd("~/Desktop")
 # Windowds: "C:\\path\Downloads" -> "C://path/Downloads"
 
 getwd()
 
-scotland <- rast("scotland_outerhebrides_oli_20240918_lrg.jpg")
-plotRGB(scotland, r=1, g=2, b=3)
+s <- rast("nzwhiteisland_oli2_20250107_lrg.jpg")
+plotRGB(s, r=1, g=2, b=3)
+
+# crop
+ext <- c(100, 1000, 100, 1000)
+sc <- crop(s, ext)
+plot(sc)
+
+# export
+writeRaster(sc, "sc.png")
 
 # Exercise: download a set of the Earth Observatory and upload it in R
 # Any ecological process is ok
