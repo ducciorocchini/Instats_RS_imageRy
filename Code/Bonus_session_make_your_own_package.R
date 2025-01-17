d9c6832eac26b08e412f4f64c9fb3eb46c48359e
Duccio Rocchini <ducciorocchini@gmail.com>
2025-01-17 13:05:24 +0100

    Create Bonus_session_make_your_own_package.R

---
+++ Code/Bonus_session_make_your_own_package.R
@@ -0,0 +1,18 @@
+# Steps to make your own package in GitHub
+
+> Caution! Secret! You just need few things to let your package work!
+
++ New repo and add README.md file
++ Put code in a R folder
+
+> Mandatory Files to be added
++ DESCRIPTION
+
+> Example
++ Look at the [testp](https://github.com/ducciorocchini/testp) package in the GitHUb account of ducciorocchini
+
+Your package is now on! You just need to install it by:
+
+```r
+devtools::install_github("ducciorocchini/packagetest")
+```
